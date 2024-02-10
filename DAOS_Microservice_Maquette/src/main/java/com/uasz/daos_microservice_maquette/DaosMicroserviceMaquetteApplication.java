54a239d5683e2fe8c3f445448f766db396af3b40
Mhdiagne <mhdiagne77@gmail.com>
2024-02-10 18:44:33 +0000

    Mise à jour de l'initialisation du projet avec l'ajout du microservice Maquette

---
+++ DAOS_Microservice_Maquette/src/main/java/com/uasz/daos_microservice_maquette/DaosMicroserviceMaquetteApplication.java
@@ -0,0 +1,13 @@
+package com.uasz.daos_microservice_maquette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaosMicroserviceMaquetteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaosMicroserviceMaquetteApplication.class, args);
+    }
+
+}
---
+++ DAOS_Microservice_Maquette/src/test/java/com/uasz/daos_microservice_maquette/DaosMicroserviceMaquetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasz.daos_microservice_maquette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaosMicroserviceMaquetteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
